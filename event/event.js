f9fc3d4b2a2e39a692417a26e34860e942b7bb2c
rabby159 <ablazerabbi159@gmail.com>
2025-02-19 11:55:17 +0600

    onClick event

--- event/event.js
+++ event/event.js
@@ -1,1 +1,12 @@
-console.log('check');
+console.log("check");
+
+function red() {
+  document.body.style.backgroundColor = "red";
+}
+
+function green() {
+  document.body.style.backgroundColor = "green";
+}
+function blue() {
+  document.body.style.backgroundColor = "blue";
+}
